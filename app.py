fdc03ed8b90d1cf347b0d4d55f9bf5ec5f85aae8
Taggu1 <hsham1270244@gmail.com>
2026-03-03 21:00:49 +0200

    another commit

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ import joblib
 # 1. Load the saved model
 model = joblib.load('kanye_model.pkl')
 
-# 2. Build the UI
+
 st.title("Ye-mometer: Is it Kanye?")
+col1, col2 = st.columns([1,2])
+
+with col1:
+    st.image("ye_photo.jpeg", caption="The Ye-mometer V1.0", width=200)
+
+with col2:
+    st.write("From 'College Dropout' to 'Vultures,' Kanye West has one of the most distinct vocabularies in music history. This project uses Natural Language Processing (NLP) to analyze linguistic patterns—like his specific use of pronouns, religious imagery, and capitalization—to determine if a phrase was born from the mind of Ye or somewhere else entirely.")
+    st.write("This classifier was built using a Multinomial Naive Bayes model. It works by calculating the 'weighted importance' of words using TF-IDF (Term Frequency-Inverse Document Frequency). Essentially, the model ignores common 'filler' words and focuses on the high-impact terms that make a sentence uniquely 'Kanye-esque.")
+st.divider()
 st.write("Type a phrase below to see if it sounds like Kanye West.")
 
 user_input = st.text_input("Enter phrase:")
